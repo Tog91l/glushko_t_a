16ca6beb3fc6e5341c7bc727ecd959906446b259
Tog91l <114440739+Tog91l@users.noreply.github.com>
2022-12-27 10:18:04 +0300

    Create bpm_22_3x.cpp

---
+++ bpm_22_3x.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+int h,w,cws,cwc,a;
+
+//cws против, cwc по часовой
+int main()
+{
+    std::string h;
+    std::string w;
+    int arr[10];
+    h = arr[10];
+    w = arr[10];
+    std::cin >> h >> w;
+    a = 1; // a последнее число a = h*w - (h*w+1)
+    if (h== w) {
+        std::cout << arr[10];
+    }
+    else {
+        std::cout << 0h << 00w;
+    }
+
+}
+
